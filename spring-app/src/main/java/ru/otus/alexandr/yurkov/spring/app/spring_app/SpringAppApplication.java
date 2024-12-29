ff523c39a044a4bc6c47ff2b6477e2925cc8fb01
aleksandryurkov <Aleksandr.Yurkov@infotecs.ru>
2024-12-29 22:02:56 +0300

    0

---
+++ spring-app/src/main/java/ru/otus/alexandr/yurkov/spring/app/spring_app/SpringAppApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.alexandr.yurkov.spring.app.spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+}
---
+++ spring-app/src/test/java/ru/otus/alexandr/yurkov/spring/app/spring_app/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.alexandr.yurkov.spring.app.spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
